7f7913085c3679c9bdd8d9d22443c932788026da
Ermali03 <52833453+Ermali03@users.noreply.github.com>
2025-04-25 11:06:37 +0200

    UPdateed icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,27 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { Header } from "@/components/header"
-import { Footer } from "@/components/footer"
-import ScrollToTop from "@/components/scroll-to-top"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
+import ScrollToTop from "@/components/scroll-to-top";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "LUDent | Premium Dental & Aesthetic Clinic",
   description:
     "LUDent is a modern dental clinic where advanced technologies are combined with an individual approach to each patient.",
-    generator: 'v0.dev'
-}
+  generator: "v0.dev",
+  icons: {
+    icon: "/images/icon.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -29,5 +32,5 @@ export default function RootLayout({
         <Footer />
       </body>
     </html>
-  )
+  );
 }
